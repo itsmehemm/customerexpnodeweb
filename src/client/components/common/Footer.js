6ef2bbf1230e974c606c2389d0e36722bdb6a85e
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-24 21:44:05 +0530

    Create Footer.js

---
+++ src/client/components/common/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <div className="footer">
+            <div className="content">
+                &copy; 2020 Tinnat Inc. All rights reserved.
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
