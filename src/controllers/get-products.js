5ae7493046d8de4b517a17dd298c6e1bf4dee70b
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-24 20:56:11 +0530

    get product controller

---
+++ src/controllers/get-products.js
@@ -0,0 +1,30 @@
+const { GET_PRODUCT_CONTROLLER } = require('../lib/constants/logging-constants');
+const { COLLECTION } = require('../lib/constants/mongo-constants');
+const mongoClient = require('../mongo/mongodb');
+
+const getProducts = (req, res) => {
+    
+    console.log(GET_PRODUCT_CONTROLLER, `Processing request to get products.`)
+
+    return mongoClient.connection(db => {
+        db.
+            collection(COLLECTION.PRODUCT)
+            .find()
+            .toArray()
+            .then(result => {
+                res.status(200).send({
+                    products: result
+                });
+            })
+            .catch(err => {
+                console.log(ADD_PRODUCT_CONTROLLER, `Error adding product to database: ${JSON.stringify(err)}`);
+                res.status(500).send({
+                    error: {
+                        message: 'INTERNAL_SERVER_ERROR',
+                        description: 'An internal server error occurred while trying to process your request. Please try again.'
+                    }
+                })
+            });
+    });
+};
+module.exports = getProducts;
