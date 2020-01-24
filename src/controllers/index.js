7650c1b652b52eed2e2eeeb38a305eca9963edf1
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-24 21:41:52 +0530

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,7 @@
 module.exports = {
     apiAuthenticator: require('./api-authenticator'),
     addProduct: require('./add-product'),
+    removeProductById: require('./remove-product-id'),
+    getProductById: require('./get-product-by-id'),
     getProducts: require('./get-products'),
 }
