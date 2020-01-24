69793c76b1d547c2f2be13a09d18b98fd1bfa0c3
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-24 20:55:40 +0530

    Create add-product-obj.js

---
+++ src/lib/db-objects/add-product-obj.js
@@ -0,0 +1,24 @@
+const addProductObj = (data) => {
+    return ({
+        name: data.name,
+        description: data.description,
+        code: data.code || 'CODE_MISC',
+        category_id: data.category_id || 'CATEGORY_MISC',
+        default_size: data.default_size,
+        default_color: data.default_color,
+        available_sizes: data.available_sizes,
+        available_colors: data.available_colors,
+        discount: {
+            type: data.discount && data.discount.type || 'NO_DISCOUNT',
+            value: data.discount && data.discount.value || 0
+        },
+        stock_quantity: data.stock_quantity || 'UNLIMITED',
+        cost: {
+            amount: data.cost && data.cost.amount || 0,
+            currency: data.cost && data.cost.currency || 'INR'
+        },
+        picture_links: data.picture_links || []
+    });
+};
+
+module.exports = addProductObj;
