0fad77e1f337cfd1783b75a532af95459de8d95d
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-24 21:38:57 +0530

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-const { addProduct, getProducts } = require('../controllers');
+const { addProduct, getProductById, removeProductById, getProducts } = require('../controllers');
 
 app.post('/product/add', addProduct)
+
 app.get('/product', getProducts)
+
+app.get('/product/:id', getProductById)
+
+app.post('/product/:id/remove', removeProductById)
 
 module.exports = app;
