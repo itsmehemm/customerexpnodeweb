fbd06be3d51a9e7d4800a92cce218dbaca44b4a3
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-25 12:20:03 +0530

    Update api-authenticator.js

--- src/controllers/api-authenticator.js
+++ src/controllers/api-authenticator.js
@@ -1,12 +1,16 @@
 const { API_AUTHENTICATOR } = require('../lib/constants/logging-constants');
-const { ADMIN_USER, ADMIN_USER_KEY } = require('../lib/constants');
+const { TINNAT_WEB_GUEST, TINNAT_WEB_GUEST_KEY, ADMIN_USER, ADMIN_USER_KEY } = require('../lib/constants');
 
 const apiAuthenticator = (req, res, next) => {
     console.log(API_AUTHENTICATOR, `Authenticating API request: ${req.method}: ${req.url}`);
     const securityContext = JSON.parse(req.headers && req.headers['x-tinnat-security-context'] || '{}');
     console.log(API_AUTHENTICATOR, `Request received from user: ${securityContext.userId}`);
     if (securityContext.userId === ADMIN_USER && securityContext.key === ADMIN_USER_KEY) {
-        console.log(API_AUTHENTICATOR, `Authorized user: ${securityContext.userId}`);
+        console.log(API_AUTHENTICATOR, `GET and POST Authorized user: ${securityContext.userId}`);
+        return next();
+    }
+    if (req.method === 'GET' && securityContext.userId === TINNAT_WEB_GUEST && securityContext.key === TINNAT_WEB_GUEST_KEY) {
+        console.log(API_AUTHENTICATOR, `GET Authorized user: ${securityContext.userId}`);
         return next();
     }
     console.log(API_AUTHENTICATOR, `User not authorized: ${securityContext.userId}`);
