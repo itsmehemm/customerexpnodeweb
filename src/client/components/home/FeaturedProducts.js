7fb8495f772ab6fa94d350a7d0c0fe76148a6240
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-26 01:33:55 +0530

    Update FeaturedProducts.js

--- src/client/components/home/FeaturedProducts.js
+++ src/client/components/home/FeaturedProducts.js
@@ -16,35 +16,35 @@ export default class FeaturedProducts extends Component {
 
     render() {
         return (
-            <div className="content">
-                <div className="header">
-                    <span className="header-large">
-                        Featured Products
-                    </span>
-                </div>
-                <div className="content-wrapper">
-                    {
-                        this.state.featured.map((product, key) =>
-                            <div onClick={() => window.open(`/product/${product.id}`)} key={key} className="l-item">
-                                <div className="p-widget">
-                                    <img src={product.picture_links[0]} height="200px" width="200px" />
-                                    <div className="p-widget-header">
-                                        {product.name}
+            <div className="in-content-wrapper">
+                <div className="content">
+                    <div className="content-header">
+                        <span className="header-large">
+                            Featured Products
+                        </span>
+                    </div>
+                    <div className="in-content-wrapper">
+                        {
+                            this.state.featured.map((product, key) =>
+                                <div onClick={() => window.open(`/product/${product.id}`)} key={key} className="l-item">
+                                    <div className="p-widget">
+                                        <img src={product.picture_links[0]} height="200px" width="200px" />
+                                        <div className="p-widget-header">
+                                            {product.name}
+                                        </div>
+                                        <div className="p-widget-info">
+                                            {product.cost.currency} {product.cost.amount}
+                                        </div>
                                     </div>
-                                    <div className="p-widget-info">
-                                        <span className="text-large-bold">
-                                            ₹ {product.cost.amount}
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>)
-                    }
+                                </div>)
+                        }
 
-                    {
-                        this.state.featured.length === 0 &&
-                        <div> There are no featured products</div>
-                    }
-                    <div className="clear"></div>
+                        {
+                            this.state.featured.length === 0 &&
+                            <div> There are no featured products</div>
+                        }
+                        <div className="clear"></div>
+                    </div>
                 </div>
             </div>
         );
