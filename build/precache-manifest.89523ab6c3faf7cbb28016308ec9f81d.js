027774dc5f24cbe09794cdc0d5e643bcff6d5e47
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-25 17:18:47 +0530

    v1.0

---
+++ build/precache-manifest.89523ab6c3faf7cbb28016308ec9f81d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944a03ba9266cbdaf926d21a1a74482a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e023063eb9d1f94af5f4",
+    "url": "/static/css/main.fa8b4bbb.chunk.css"
+  },
+  {
+    "revision": "0cc9ffb5a544c5af0ace",
+    "url": "/static/js/2.9326c15b.chunk.js"
+  },
+  {
+    "revision": "ea538b5324860afec8786da620cef98b",
+    "url": "/static/js/2.9326c15b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e023063eb9d1f94af5f4",
+    "url": "/static/js/main.7922f282.chunk.js"
+  },
+  {
+    "revision": "310c57964bee6497d843",
+    "url": "/static/js/runtime-main.b4fad7ba.js"
+  },
+  {
+    "revision": "0a9a5ccda3bd7c06089042b750eab4c9",
+    "url": "/static/media/banner.0a9a5ccd.jpg"
+  },
+  {
+    "revision": "4d7196b311f6dc6b17cdcd380919121b",
+    "url": "/static/media/tinnat-logo.4d7196b3.png"
+  },
+  {
+    "revision": "9e44ceb5599e2c78e2cd785c817e9b3c",
+    "url": "/static/media/tshirt.9e44ceb5.jpg"
+  }
+]);
