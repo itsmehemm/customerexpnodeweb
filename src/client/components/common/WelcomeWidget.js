ced80a01e48a741b3730967820aea22c0a9ce4cc
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-25 16:43:56 +0530

    Create WelcomeWidget.js

---
+++ src/client/components/common/WelcomeWidget.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const WelcomeWidget = () => {
+    return (
+        <div className="content-wrapper">
+            <div className="content">
+                <div className="header">
+                    <span className="header-large">
+                        Welcome to Tinnat.com
+                </span>
+                </div>
+                <div className="text-content">
+                    Tinnat.com, a.k.a The Innovative Attire is an innovative e-commerce website built in and for Indian customers with love. Shop for T-shirts, Jeans, Accessories and many other stuffs for the best prices and quality.
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default WelcomeWidget;
