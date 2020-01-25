f714038d88b76000c4cf854f20172fd524f5e77d
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-25 16:43:20 +0530

    Update Tinnat.js

--- src/client/Tinnat.js
+++ src/client/Tinnat.js
@@ -2,8 +2,11 @@
 import React, { Component } from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/common/Header.js'
+import Footer from './components/common/Footer.js'
 import Product from './components/product/Product';
 import './styles/index.css';
+
 export default class Tinnat extends Component {
 
     constructor(props) {
@@ -13,8 +16,10 @@ export default class Tinnat extends Component {
     render() {
         return (
             <Router>
+                <Header />
                 <Route exact path="/" component={Home} />
                 <Route path="/product/:productid" component={Product} />
+                <Footer />
             </Router>
         )
     }
