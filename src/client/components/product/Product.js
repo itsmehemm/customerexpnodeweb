dbfa02605a7468b02bd573dffa61259184374cf6
itsmehemm <hemanthprasathmurali@gmail.com>
2020-01-26 01:34:01 +0530

    Update Product.js

--- src/client/components/product/Product.js
+++ src/client/components/product/Product.js
@@ -23,62 +23,56 @@ export default class Product extends Component {
 
         if (productinfo)
             return (
-                <div>
-                    <div className="content-wrapper">
-                        <div className="content">
-                            <div className="p-content">
-                                <div className="p-images">
-                                    <img src={productinfo && productinfo.picture_links && productinfo.picture_links[0] || tshirt} height="400px" width="400px" />
-                                </div>
-                                <div className="p-infos">
-                                    <div className="p-item">
-                                        <span className="header-text-large">{productinfo.name}</span>
-                                    </div>
-
-                                    <div className="p-item">
-                                        <span className="header-text-medium">{productinfo.description}</span>
-                                    </div>
-
-                                    <div className="p-item">
-                                        <Amount cost={productinfo.cost} discount={productinfo.discount} />
-                                    </div>
-                                    <div className="p-item">
-                                        <SmallButtonGroup
-                                            onSelect={() => { }}
-                                            defaultButton={productinfo.default_size}
-                                            buttons={productinfo.available_sizes}
-                                        />
-                                    </div>
-                                    <div className="p-item">
-                                        <SmallButtonGroup
-                                            onSelect={() => { }}
-                                            defaultButton={productinfo.default_color}
-                                            buttons={productinfo.available_colors}
-                                        />
-                                    </div>
-                                    <div className="p-item">
-                                        {
-                                            parseInt(productinfo.stock_quantity) > 0 ?
-                                                <span className="small-header-text green-text">
-                                                    <i className="material-icons">done</i>
-                                                    &ensp; IN STOCK
-                                                    </span> :
-                                                <span className="small-header-text red-text">
-                                                    <i className="material-icons">cancel</i>
-                                                    &ensp; OUT OF STOCK
-                                                    </span>
-                                        }
-                                    </div>
-                                    <div className="p-item">
-                                        <button className="add-cart-btn">
-                                            <i className="material-icons">add_shopping_cart</i>
-                                            &ensp;ADD TO CART </button>
-                                    </div>
-
-                                </div>
-                                <div className="clear" />
-                            </div>
+                <div className="in-content-wrapper">
+                    <div className="content">
+                        <div className="p-images">
+                            <img src={productinfo && productinfo.picture_links && productinfo.picture_links[0] || tshirt} height="400px" width="400px" />
                         </div>
+                        <div className="p-infos">
+                            <div className="p-item">
+                                <span className="header-large">{productinfo.name}</span>
+                            </div>
+                            <div className="p-item">
+                                <span className="header-text-medium">{productinfo.description}</span>
+                            </div>
+                            <div className="p-item">
+                                <Amount cost={productinfo.cost} discount={productinfo.discount} />
+                            </div>
+                            <div className="p-item">
+                                <SmallButtonGroup
+                                    onSelect={() => { }}
+                                    defaultButton={productinfo.default_size}
+                                    buttons={productinfo.available_sizes}
+                                />
+                            </div>
+                            <div className="p-item">
+                                <SmallButtonGroup
+                                    onSelect={() => { }}
+                                    defaultButton={productinfo.default_color}
+                                    buttons={productinfo.available_colors}
+                                />
+                            </div>
+                            <div className="p-item">
+                                {
+                                    parseInt(productinfo.stock_quantity) > 0 ?
+                                        <span className="small-header-text green-text">
+                                            <i className="material-icons">done</i>
+                                            &ensp; IN STOCK
+                                                    </span> :
+                                        <span className="small-header-text red-text">
+                                            <i className="material-icons">cancel</i>
+                                            &ensp; OUT OF STOCK
+                                                    </span>
+                                }
+                            </div>
+                            <div className="p-item">
+                                <button className="add-cart-btn">
+                                    <i className="material-icons">add_shopping_cart</i>
+                                    &ensp;ADD TO CART </button>
+                            </div>
+
+                        </div>
+                        <div className="clear" />
                     </div>
                 </div>
             )
