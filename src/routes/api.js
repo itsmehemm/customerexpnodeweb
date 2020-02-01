2a7a203a4901b10c77b85890a17e865695895f7e
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-02 01:52:47 +0530

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const app = express();
 
-const { addProduct, getProductById, editProductById, removeProductById, getFeaturedProducts, getProducts } = require('../controllers');
+const {
+    addProduct,
+    getProductById,
+    editProductById,
+    removeProductById,
+    getFeaturedProducts,
+    getProducts,
+    addProductToCart,
+    editProductInCart,
+    removeProductFromCart,
+    getCart
+} = require('../controllers');
 
-app.get('/products', getProducts)
+app.get('/products', getProducts);
 
-app.get('/products/featured', getFeaturedProducts)
+app.get('/products/featured', getFeaturedProducts);
 
-app.post('/product/add', addProduct)
+app.post('/product/add', addProduct);
 
-app.get('/product/:id', getProductById)
+app.get('/product/:id', getProductById);
 
-app.post('/product/:id/update', editProductById)
+app.post('/product/:id/update', editProductById);
 
-app.post('/product/:id/remove', removeProductById)
+app.post('/product/:id/remove', removeProductById);
 
+app.post('/cart/add', addProductToCart);
 
+app.post('/cart/edit', editProductInCart);
+
+app.post('/cart/remove', removeProductFromCart);
+
+app.get('/cart', getCart);
 
 module.exports = app;
