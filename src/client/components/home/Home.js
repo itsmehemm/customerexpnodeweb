aa7e74a9b7974a987bddf7f6edabc8283c0bc555
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 17:19:36 +0530

    Update Home.js

--- src/client/components/home/Home.js
+++ src/client/components/home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Header from '../common/Header.js'
 import HomeBanner from '../common/HomeBanner';
 import FeaturedProducts from '../home/FeaturedProducts';
 import WelcomeWidget from '../common/WelcomeWidget'
+import Footer from '../common/Footer.js'
 
 const Home = () => {
     return (
         <div>
+            <Header />
             <HomeBanner />
             <FeaturedProducts />
             <WelcomeWidget />
+            <Footer />
         </div>
     )
 }
