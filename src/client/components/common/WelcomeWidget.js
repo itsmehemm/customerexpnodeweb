9d546031250a18710ce51213504e0c929bb5e9d9
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 17:35:49 +0530

    Update WelcomeWidget.js

--- src/client/components/common/WelcomeWidget.js
+++ src/client/components/common/WelcomeWidget.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+
+import Typography from '../common/elements/Typography';
 
 const WelcomeWidget = () => {
     return (
-        <div className="content-wrapper">
-            <div className="content">
-                <div className="header">
+        <Container maxWidth={"lg"}>
+            <Grid alignItems="center"
+                justify="center"
+                container
+                spacing={1}>
+                <Box m={4}>
                     <span className="header-large">
-                        Welcome to <span className="highlight-text">T</span>he <span className="highlight-text">INN</span>ovative <span className="highlight-text">AT</span>tire
-                </span>
-                </div>
-                <div className="text-content">
-                    Tinnat.com, a.k.a The Innovative Attire is an innovative e-commerce website built in and for Indian customers with love. Shop for T-shirts, Jeans, Accessories and many other stuffs for the best prices and quality.
-            </div>
-            </div>
-        </div>
+                        Welcome to&nbsp;
+                        <span className="highlight-text">T</span>
+                        he&nbsp;
+                        <span className="highlight-text">INN</span>
+                        ovative&nbsp;
+                        <span className="highlight-text">AT</span>
+                        tire
+                    </span>
+                </Box>
+                <Typography size="subtitle1" text="Tinnat.com, a.k.a, The Innovative Attire is an innovative e-commerce website built in and for Indian customers with love. Shop for T-shirts, Jeans, Accessories and many other stuffs for the best prices and quality." />
+            </Grid>
+        </Container>
     );
 };
 
