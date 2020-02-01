00233e630ae468d9ed3509227a5f119205415bd4
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 18:26:26 +0530

    Create ProductDetailLoading.js

---
+++ src/client/components/product-detail/ProductDetailLoading.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+
+const ProductDetailLoading = (props) => {
+    return (
+        <Container maxWidth="sm">
+            <Grid container justify="center">
+                <Box m={35}><CircularProgress /></Box>
+            </Grid>
+        </Container>
+    );
+};
+
+export default ProductDetailLoading;
