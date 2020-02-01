769198605318c32b10db2f4b430e6f2972ff02bf
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 18:27:09 +0530

    Create add-product.js

---
+++ src/client/actions/add-product.js
@@ -0,0 +1,24 @@
+import request from 'request';
+import config from '../configs/config.json';
+
+const environment = 'production';
+// const environment = 'development';
+
+const addProduct = async (product) => {
+    return new Promise((resolve, reject) => {
+        request({
+            uri: config[environment].api.v1_post_add_product.uri,
+            method: 'POST',
+            headers: {
+                'X-TINNAT-SECURITY-CONTEXT': JSON.stringify({ "userId": "admin", "key": "tinnat" })
+            },
+            body: product
+        }, (e, r, b) => {
+            b = JSON.parse(b || '{}');
+            console.log(JSON.stringify(b));
+            resolve(b);
+        });
+    });
+};
+
+export default addProduct;
