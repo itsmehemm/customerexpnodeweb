4d8b8f65e4c26da0dfa1e223ef2c768b2de234bf
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 17:18:12 +0530

    Update Footer.js

--- src/client/components/common/Footer.js
+++ src/client/components/common/Footer.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+import Typography from '../common/elements/Typography';
+
 import '../../styles/footer.css';
 
-const Footer = (props) => {
+const Footer = () => {
     return (
-        <div className="footer">
-            &copy; 2020 Tinnat Inc. All rights reserved.
-        </div>
-    )
-}
+        <Container maxWidth="xl">
+            <Box m={2}>
+                <Typography align="center" size="subtitle1" text="&copy; 2020 Tinnat Inc. All rights reserved." />
+            </Box>
+        </Container>
+    );
+};
 
 export default Footer;
