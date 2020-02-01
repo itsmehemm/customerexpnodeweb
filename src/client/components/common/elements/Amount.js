dc47d5edae37d8c17b434351b92812285828a3d9
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 18:26:33 +0530

    Update Amount.js

--- src/client/components/common/elements/Amount.js
+++ src/client/components/common/elements/Amount.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { currencyCodeLabel } from '../../../client-lib/mappers';
 
 const Amount = (props) => {
 
@@ -16,14 +17,14 @@ const Amount = (props) => {
 
     return (
         <div className="amount-section">
-            <span className="final-amount-text"> {cost.currency} {discountedAmount} </span>
+            <span className="final-amount-text"> {currencyCodeLabel[cost.currency]}{discountedAmount} </span>
             &ensp;
-            <span className="original-amount-text">{cost.currency} {amount}</span>
+            <span className="original-amount-text">{currencyCodeLabel[cost.currency]}{amount}</span>
             &ensp;
             <span className="amount-offer-text">
                 &nbsp;{
                     discount.type === 'INSTANT' ?
-                        `${cost.currency} ${discountValue} off` :
+                        `Flat ${currencyCodeLabel[cost.currency]}${discountValue} off` :
                         `${discountValue}% off`
                 }
             </span>
