2516c53f142d5922b9726246ed004f8d760be6b7
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 18:25:59 +0530

    Update FeaturedProducts.js

--- src/client/components/home/FeaturedProducts.js
+++ src/client/components/home/FeaturedProducts.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '../common/elements/Typography';
+import Amount from '../common/elements/Amount';
 import getFeaturedProducts from '../../actions/get-featured-products';
+import { currencyCodeLabel } from '../../client-lib/mappers';
 
 export default class FeaturedProducts extends Component {
     constructor(props) {
@@ -16,37 +22,35 @@ export default class FeaturedProducts extends Component {
 
     render() {
         return (
-            <div className="in-content-wrapper">
-                <div className="content">
-                    <div className="content-header">
-                        <span className="header-large">
-                            Featured Products
-                        </span>
-                    </div>
-                    <div className="in-content-wrapper">
-                        {
-                            this.state.featured.map((product, key) =>
-                                <div onClick={() => window.open(`/product/${product.id}`)} key={key} className="l-item">
-                                    <div className="p-widget">
-                                        <img src={product.picture_links[0]} height="200px" width="200px" />
-                                        <div className="p-widget-header">
-                                            {product.name}
-                                        </div>
-                                        <div className="p-widget-info">
-                                            {product.cost.currency} {product.cost.amount}
-                                        </div>
-                                    </div>
-                                </div>)
-                        }
-
-                        {
-                            this.state.featured.length === 0 &&
-                            <div> There are no featured products</div>
-                        }
-                        <div className="clear"></div>
-                    </div>
-                </div>
-            </div>
+            <Container maxWidth={"lg"}>
+                <Box m={1}>
+                    <Box className="center" m={4}>
+                        <span className="header-large"> Featured Products </span>
+                    </Box>
+                    <Box className="center" m={4}>
+                        <Grid container spacing={1}>
+                            {
+                                this.state.featured.map((product, key) =>
+                                    <Grid item xs={4} key={key} onClick={() => window.open(`/product/${product.id}`)} >
+                                        <Box m={1} className="p-widget">
+                                            <img src={product.picture_links[0]} height="250px" width="250px" />
+                                            <Box m={1}>
+                                                <Typography text={product.name} size="subtitle1" />
+                                            </Box>
+                                            <Box m={1}>
+                                                <Amount cost={product.cost} discount={product.discount} />
+                                            </Box>
+                                        </Box>
+                                    </Grid>)
+                            }
+                            {
+                                this.state.featured.length === 0 &&
+                                <div> There are no featured products </div>
+                            }
+                        </Grid>
+                    </Box>
+                </Box >
+            </Container >
         );
     }
 }
