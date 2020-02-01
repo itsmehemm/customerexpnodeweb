ef774d657d69f06c88c8dd624915a073979e2b56
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 18:25:07 +0530

    Create MultiSelect.js

---
+++ src/client/components/common/elements/MultiSelect.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Input from '@material-ui/core/Input';
+import Checkbox from '@material-ui/core/Checkbox';
+import ListItemText from '@material-ui/core/ListItemText';
+import InputLabel from '@material-ui/core/InputLabel';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const MultiSelect = (props) => {
+    const { id, options = [], error = false, label, helperText = "", required = false, values = [], type = "text", width = 12, onChange } = props;
+    return (
+        <Grid item xs={width}>
+            <InputLabel>{label}</InputLabel>
+            <Select
+                error={error}
+                fullWidth={true}
+                variant="outlined"
+                id={id}
+                multiple
+                value={values}
+                onChange={event => onChange(event.target.value)}
+                input={<Input />}
+                renderValue={selected => selected.join(', ')}
+                MenuProps={MenuProps}
+            >
+                {options.map(name => (
+                    <MenuItem key={name} value={name}>
+                        <Checkbox checked={values.indexOf(name) > -1} />
+                        <ListItemText primary={name} />
+                    </MenuItem>
+                ))}
+            </Select>
+            <InputLabel>{helperText}</InputLabel>
+        </Grid>);
+}
+
+export default MultiSelect;
+
+
+
