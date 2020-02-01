d55c962adf41c2db22ba4d04c20426ab924de6f3
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-02 01:53:16 +0530

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,4 +6,11 @@ module.exports = {
     getProductById: require('./get-product-by-id'),
     getFeaturedProducts: require('./get-featured-products'),
     getProducts: require('./get-products'),
+
+    addProductToCart: require('./cart/add-product-to-cart'),
+    removeProductFromCart: require('./cart/remove-product-from-cart'),
+    editProductInCart: require('./cart/edit-product-in-cart'),
+    getCart: require('./cart/get-cart'),
+
+    validateUserSession: require('./validate-user-session')
 }
