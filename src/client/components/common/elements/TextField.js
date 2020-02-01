1ef42a954feea793d2f28b654e389117512e7da0
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 18:25:17 +0530

    Create TextField.js

---
+++ src/client/components/common/elements/TextField.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+
+const _TextField = (props) => {
+    const { id, label, helperText = "", error = false, required = false, value = '', type = "text", width = 12, onChange } = props;
+    return (
+        <Grid item xs={width}>
+            <TextField
+                error={error}
+                required={required}
+                value={value}
+                fullWidth={true}
+                id={id}
+                label={label}
+                type={type}
+                helperText={helperText}
+                onChange={event => onChange(event.target.value)}
+                variant="outlined"
+            />
+        </Grid>
+    );
+}
+
+export default _TextField;
