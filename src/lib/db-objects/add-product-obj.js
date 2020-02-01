a4a1871f1b9eee89956cd23314590d84f8fee9f5
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-01 15:51:50 +0530

    Update add-product-obj.js

--- src/lib/db-objects/add-product-obj.js
+++ src/lib/db-objects/add-product-obj.js
@@ -21,7 +21,26 @@ const addProductObj = (data) => {
             currency: data.cost && data.cost.currency || 'INR'
         },
         picture_links: data.picture_links || [],
-        featured: data.featured || false
+        featured: data.featured || false,
+        thirty_day_exchange: data.thirty_day_exchange || false,
+        fifteen_day_exchange: data.fifteen_day_exchange || false,
+        payment_options: {
+            cash_on_delivery: data.payment_options && data.payment_options.cash_on_delivery,
+            credit_card: data.payment_options && data.payment_options.credit_card
+        },
+        details: {
+            type: data.details && data.details.type,
+            sleeve: data.details && data.details.sleeve,
+            fit: data.details && data.details.fit,
+            fabric: data.details && data.details.fabric,
+            pack_size: data.details && data.details.pack_size,
+            neck_type: data.details && data.details.neck_type,
+            ideal_gender: data.details && data.details.ideal_gender,
+            occasion: data.details && data.details.occasion,
+            brand_color: data.details && data.details.brand_color,
+            fabric_care: data.details && data.details.fabric_care,
+            brand_fit: data.details && data.details.brand_fit,
+        }
     });
 };
 
