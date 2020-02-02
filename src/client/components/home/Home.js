22c4e9397445eb83383760aad30834c0b9ce7d4c
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-02 09:41:51 +0530

    Update Home.js

--- src/client/components/home/Home.js
+++ src/client/components/home/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../common/Header.js'
 import HomeBanner from '../common/HomeBanner';
 import FeaturedProducts from '../home/FeaturedProducts';
-import WelcomeWidget from '../common/WelcomeWidget'
 import Footer from '../common/Footer.js'
 
 const Home = () => {
