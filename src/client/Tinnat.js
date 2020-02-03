0fc7f976ccf81a8dd9ed5b07762196435022c673
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-03 23:04:13 +0530

    Update Tinnat.js

--- src/client/Tinnat.js
+++ src/client/Tinnat.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/home/Home';
 import Cart from './components/cart/Cart';
+import Checkout from './components/checkout/Checkout';
 import ProductDetailWrapper from './components/product-detail/ProductDetailWrapper';
 import './styles/index.css';
 
@@ -19,6 +20,7 @@ export default class Tinnat extends Component {
                 <Route exact path="/home" component={Home} />
                 <Route path="/product/:productid" component={ProductDetailWrapper} />
                 <Route path="/viewcart" component={Cart} />
+                <Route path="/checkout" component={Checkout} />
                 {/* <Route path="/admin/product/add" component={ProductDetails} /> */}
             </Router>
         );
