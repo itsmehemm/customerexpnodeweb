f644b676fc21b3cdd735bc6bb40f873684cc55a3
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-03 23:02:48 +0530

    Create add-item-cart.js

---
+++ src/client/actions/cart/add-item-cart.js
@@ -0,0 +1,27 @@
+import config from '../../configs/config.json';
+
+// const environment = 'production';
+const environment = 'development';
+
+export const addItemToCart = async (item) => {
+    return new Promise((resolve, reject) => {
+        fetch(config[environment].api.v1_post_add_item_cart.uri, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-TINNAT-SECURITY-CONTEXT': JSON.stringify({ "userId": "admin", "key": "tinnat" })
+            },
+            body: JSON.stringify(item)
+        })
+            .then(response => response.json())
+            .then(response => {
+                console.log(JSON.stringify(response));
+                resolve(response);
+            }).catch(error => {
+                resolve({})
+            });
+    });
+};
+
+export default addItemToCart;
