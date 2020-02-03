80ace3a9c467fe9e38187a21007ebd2b8b910960
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-03 23:03:05 +0530

    Create index.js

---
+++ src/client/actions/cart/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    addItemToCart: require('./add-item-cart'),
+    removeItemFromCart: require('./remove-item-cart'),
+    editItemInCart: require('./edit-item-cart'),
+    getCart: require('./get-cart')
+};
