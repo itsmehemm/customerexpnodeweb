645eeca9eff59e0761df054dee13f77bff29619b
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-03 23:02:51 +0530

    Create edit-item-cart.js

---
+++ src/client/actions/cart/edit-item-cart.js
@@ -0,0 +1,24 @@
+import request from 'request';
+import config from '../../configs/config.json';
+
+const environment = 'production';
+// const environment = 'development';
+
+const editItemInCart = async (item) => {
+    return fetch(config[environment].api.v1_post_edit_item_cart.uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-TINNAT-SECURITY-CONTEXT': JSON.stringify({ "userId": "admin", "key": "tinnat" })
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(response => {
+            console.log(JSON.stringify(response));
+            return response;
+        })
+};
+
+export default editItemInCart;
