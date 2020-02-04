14194ff2d9633df81b9b38a679f6283ef576ec8c
itsmehemm <hemanthprasathmurali@gmail.com>
2020-02-04 14:42:27 +0530

    Update Header.js

--- src/client/components/common/Header.js
+++ src/client/components/common/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Typography from './elements/Typography';
 
 import logo from '../../images/tinnat-logo-white.png';
 import '../../styles/header.css';
@@ -16,7 +17,7 @@ export default class Header extends Component {
         }
     }
 
-    componentDidMount() {
+    updateComponent() {
         getCart()
             .then(response => {
                 if (response && response.cart) {
@@ -28,34 +29,42 @@ export default class Header extends Component {
             .catch(error => this.setState({ cart: [] }))
     }
 
+    componentDidMount() {
+        // To update the cart periodically every 2 seconds.
+        setInterval(() => {
+            this.updateComponent();
+        }, 2000);
+    }
+
     render() {
         return (
             <Container className="header-content-wrapper" maxWidth={false}>
                 <Container maxWidth={"xl"}>
-                    <Grid className="menu" alignItems="center"
+                    <Grid className="menu"
+                        alignItems="center"
                         justify="center"
-                        container
-                        spacing={1}>
+                        container>
                         <Grid className="logo" onClick={() => window.location = '/'} item xs={1}>
                             <img src={logo} height="45px" width="80px" />
                         </Grid>
                         <Grid className="menu-item" onClick={() => window.location = '/'} item xs={1}>
-                            Home
+                            <Typography text="Home" />
                         </Grid>
                         <Grid className="menu-item" onClick={() => window.location = '/about'} item xs={1}>
-                            About
+                            <Typography text="About" />
                         </Grid>
                         <Grid className="menu-item" onClick={() => window.location = '/products'} item xs={1}>
-                            Products
+                            <Typography text="Products" />
                         </Grid>
                         <Grid className="menu-item" onClick={() => window.location = '/help'} item xs={1}>
-                            Help
+                            <Typography text="Help" />
                         </Grid>
                         <Grid className="menu-item" onClick={() => window.location = '/contact'} item xs={1}>
-                            Contact
+                            <Typography text="Contact" />
                         </Grid>
                         <Grid className="menu-item" onClick={() => window.location = '/cart'} item xs={1}>
-                            <i className="material-icons">add_shopping_cart</i>&nbsp;({this.state.cart.length})
+                            <Typography icon="add_shopping_cart" text={`(${this.state.cart.length})`} />
+
                         </Grid>
                     </Grid>
                 </Container>
