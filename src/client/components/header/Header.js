dbd8df60a6a25b49d0b1b35cbb7f143f4d48ac08
itsmehemm <hemanthprasathmurali@gmail.com>
2020-03-08 10:33:53 +0530

    Create Header.js

---
+++ src/client/components/header/Header.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+import Typography from '../common/elements/Typography';
+
+import { getCart } from '../../actions/cart/get-cart';
+
+import {
+    OPERATION_LOADING,
+    OPERATION_LOADING_COMPLETED,
+    OPERATION_LOADING_ERROR
+} from '../../client-lib/constants';
+
+import logo from '../../images/tinnat-logo-white.png';
+import '../../styles/header.css';
+
+export default class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: OPERATION_LOADING,
+            cart: []
+        }
+    }
+
+    componentDidMount() {
+        // To update the cart periodically every second.
+        setInterval(() => {
+            this.updateComponent();
+        }, 1000);
+    }
+
+    updateComponent() {
+        getCart()
+            .then(response => {
+                if (response && response.cart) {
+                    this.setState({ status: OPERATION_LOADING_COMPLETED, cart: response.cart });
+                } else {
+                    this.setState({ status: OPERATION_LOADING_ERROR, cart: [] });
+                }
+            })
+            .catch(error => this.setState({ status: OPERATION_LOADING_ERROR, cart: [] }))
+    }
+
+    render() {
+        return (
+            <Container className="header-wrapper" maxWidth={false}>
+                <Container maxWidth={"xl"}>
+                    <Grid className="menu"
+                        alignItems="center"
+                        justify="center"
+                        container>
+                        <Grid className="logo" onClick={() => window.location = '/'} item xs={1}>
+                            <img src={logo} height="45px" width="80px" />
+                        </Grid>
+                        <Grid className="menu-item" onClick={() => window.location = '/'} item xs={1}>
+                            <Typography text="Home" />
+                        </Grid>
+                        <Grid className="menu-item" onClick={() => window.location = '/about'} item xs={1}>
+                            <Typography text="About" />
+                        </Grid>
+                        <Grid className="menu-item" onClick={() => window.location = '/products'} item xs={1}>
+                            <Typography text="Products" />
+                        </Grid>
+                        <Grid className="menu-item" onClick={() => window.location = '/help'} item xs={1}>
+                            <Typography text="Help" />
+                        </Grid>
+                        <Grid className="menu-item" onClick={() => window.location = '/contactus'} item xs={1}>
+                            <Typography text="Contact" />
+                        </Grid>
+                        <Grid className="menu-item" onClick={() => window.location = '/viewcart'} item xs={1}>
+                            <Typography icon="add_shopping_cart" text={
+                                `(${this.state.status === OPERATION_LOADING ? '...' : this.state.cart.length})`
+                            } />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Container>
+        )
+    }
+}
