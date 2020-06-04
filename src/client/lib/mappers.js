9cb894007aaafbd5d27f90cd32497931a894cc9b
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-04 18:19:06 +0530

    client lib

---
+++ src/client/lib/mappers.js
@@ -0,0 +1,18 @@
+export const productAdvancedDetailsMapper =  {
+    "type": "Type",
+    "sleeve": "Sleeve",
+    "fit": "Fit",
+    "fabric": "Fabric",
+    "pack_size": "Pack Size",
+    "neck_type": "Neck Type",
+    "ideal_gender": "Ideal For",
+    "occasion": "Best occasion",
+    "brand_color": "Brand Color",
+    "fabric_care": "Fabric Care",
+    "brand_fit": "Brand Fit"
+}
+
+export const currencyCodeMapper = {
+    'INR': '₹',
+    'USD': '$'
+}
