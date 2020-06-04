ea75886eb049e0e26b068f684f5980fc95cb1e5e
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-04 18:05:11 +0530

    Create ViewProductModal.js

---
+++ src/server/modals/ViewProductModal.js
@@ -0,0 +1,50 @@
+class ViewProductModal {
+    constructor() {
+        this.data = null;
+    }
+
+    setData(d) {
+        this.data = {
+            id: d.id,
+            name: d.name,
+            description: d.description,
+            product_code: d.product_code,
+            category_code: d.category_code,
+            default_size: d.default_size,
+            default_color: d.default_color,
+            available_sizes: d.available_sizes,
+            available_colors: d.available_colors,
+            discount: {
+                type: d.discount && d.discount.type,
+                value: d.discount && d.discount.value
+            },
+            stock_quantity: d.stock_quantity,
+            cost: {
+                amount: d.cost && d.cost.amount,
+                currency: d.cost && d.cost.currency
+            },
+            picture_links: d.picture_links,
+            featured: d.featured,
+            thirty_day_exchange: d.thirty_day_exchange,
+            fifteen_day_exchange: d.fifteen_day_exchange,
+            payment_options: d.payment_options,
+            advanced_details: {
+                type: d.advanced_details && d.advanced_details.type,
+                sleeve: d.advanced_details && d.advanced_details.sleeve,
+                fit: d.advanced_details && d.advanced_details.fit,
+                fabric: d.advanced_details && d.advanced_details.fabric,
+                pack_size: d.advanced_details && d.advanced_details.pack_size,
+                neck_type: d.advanced_details && d.advanced_details.neck_type,
+                ideal_gender: d.advanced_details && d.advanced_details.ideal_gender,
+                occasion: d.advanced_details && d.advanced_details.occasion,
+                brand_color: d.advanced_details && d.advanced_details.brand_color,
+                fabric_care: d.advanced_details && d.advanced_details.fabric_care,
+                brand_fit: d.advanced_details && d.advanced_details.brand_fit,
+            }
+        }
+    }
+
+    getData() { return this.data }
+};
+
+module.exports = ViewProductModal;
