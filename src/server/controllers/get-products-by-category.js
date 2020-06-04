ded6589948e587c4b46c602a8f6f9fd25d724d76
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-04 18:05:57 +0530

    Create get-products-by-category.js

---
+++ src/server/controllers/get-products-by-category.js
@@ -0,0 +1,40 @@
+const mongoClient = require('../mongo/mongodb');
+const ViewProductModal = require('../modals/ViewProductModal');
+const { GET_PRODUCTS_BY_CATEGORY_CONTROLLER } = require('../lib/constants/logging-constants');
+const { COLLECTION } = require('../lib/constants/mongo-constants');
+
+const getProductsByCategory = (req, res) => {
+    console.log(GET_PRODUCTS_BY_CATEGORY_CONTROLLER, `Processing request to get products by category: ${JSON.stringify(req.params.category_code)}.`)
+    return mongoClient.connection(db => {
+        db.
+            collection(COLLECTION.PRODUCT)
+            .find({
+                'category_code': req.params.category_code
+            })
+            .toArray()
+            .then(result => {
+                let products = [];
+                if (Array.isArray(result) && result.length > 0) {
+                    result.forEach(product => {
+                        const viewProductModal = new ViewProductModal();
+                        viewProductModal.setData(product);
+                        products.push(viewProductModal.getData());
+                    });
+                }
+                return res.status(200).send({
+                    products: products
+                });
+            })
+            .catch(err => {
+                console.log(GET_PRODUCTS_BY_CATEGORY_CONTROLLER, `Error performing operation in database: ${JSON.stringify(err)}`);
+                res.status(500).send({
+                    error: {
+                        message: 'INTERNAL_SERVER_ERROR',
+                        description: 'An internal server error occurred while trying to process your request. Please try again.'
+                    }
+                });
+            });
+    });
+};
+
+module.exports = getProductsByCategory;
