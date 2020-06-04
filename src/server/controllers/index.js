5c6e4400fc1d576bbab930b2637fa84b5d3ebf10
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-04 18:05:53 +0530

    Create index.js

---
+++ src/server/controllers/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apiAuthenticator: require('./api-authenticator'),
+    addProduct: require('./add-product'),
+    editProductById: require('./edit-product-by-id'),
+    removeProductById: require('./remove-product-id'),
+    getProductById: require('./get-product-by-id'),
+    getFeaturedProducts: require('./get-featured-products'),
+    getProducts: require('./get-products'),
+    getProductsByCategory: require('./get-products-by-category'),
+    addProductToCart: require('./cart/add-product-to-cart'),
+    removeProductFromCart: require('./cart/remove-product-from-cart'),
+    editProductInCart: require('./cart/edit-product-in-cart'),
+    getCart: require('./cart/get-cart'),
+    validateUserSession: require('./validate-user-session'),
+    createInstantOrder: require('./create-instant-order'),
+    getOrderById: require('./get-order-by-id'),
+}
