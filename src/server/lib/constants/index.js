f1bcae51a824a3c997f4bbddc1cd817ae3503dfa
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-04 18:05:40 +0530

    Create index.js

---
+++ src/server/lib/constants/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ADMIN_USER: 'admin',
+    ADMIN_USER_KEY: 'tinnat',
+    TINNAT_WEB_GUEST: 'tinnat_guest',
+    TINNAT_WEB_GUEST_KEY: 'tinnat_guest_secret',
+    ORDER_LIFE_TIME: 14400
+};
