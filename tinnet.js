b70f8f8d7f766050ef866e526193dd5830b1ccce
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-04 16:30:12 +0530

    Update tinnet.js

--- tinnet.js
+++ tinnet.js
@@ -1,4 +1,4 @@
-const app = require('./src/routes');
-const PORT = 2002;
+const app = require('./src/server/routes');
+const PORT = process.env.PORT || 2002;
 
-app.listen(PORT, () => { console.log(`TINNAT RUNNING ON PORT ${PORT}`) });
+app.listen(PORT, () => { console.log(`TINNAT WEBAPP & API RUNNING ON PORT ${PORT}`) });
