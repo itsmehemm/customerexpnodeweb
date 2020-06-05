fb30ab88ebe11228d3f569784c2d35e10325f869
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-05 16:07:01 +0530

    Create error-constants.js

---
+++ src/server/lib/constants/error-constants.js
@@ -0,0 +1,18 @@
+module.exports = {
+    INVALID_DATA: {
+        message: 'INVALID_DATA',
+        description: 'Invalid data received in the request. Please check the API specification and try again.'
+    },
+    MISSING_REQUIRED_PARAMETERS: {
+        message: 'MISSING_REQUIRED_PARAMETERS',
+        description: 'One or more required parameters missing. Please check the API specification and try again.'
+    },
+    INTERNAL_SERVER_ERROR: {
+        message: 'INTERNAL_SERVER_ERROR',
+        description: 'An internal server error occurred while trying to process your request. Please try again.'
+    },
+    DATABASE_ERROR: {
+        message: 'DATABASE_ERROR',
+        description: 'An error occurred while executing a query in the database. Please try again.'
+    }
+};
