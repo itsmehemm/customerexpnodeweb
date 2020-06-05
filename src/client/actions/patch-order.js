dd2e4cd8f063f91b1e0a545366a1eb79c7574d73
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-05 10:17:10 +0530

    actions updated

--- src/client/actions/patch-order.js
+++ src/client/actions/patch-order.js
@@ -2,26 +2,23 @@ import config from '../configs/config.json';
 
 const environment = config.environment;
 
-const updateProductById = async (product) => {
-    console.log('[INFO]', 'action::updateProduct');
-    console.log('[INFO]', 'request', JSON.stringify(product, undefined, 4));
-
+export const patchOrder = (order) => {
+    console.log('[INFO]', 'action::patchOrder');
+    console.log('[INFO]', 'request', `order=${JSON.stringify(order, undefined, 2)}`);
     var myHeaders = new Headers();
     myHeaders.append("X-TINNAT-SECURITY-CONTEXT", "{\"userId\": \"admin\", \"key\": \"tinnat\"}");
     myHeaders.append("Content-Type", "application/json");
-
-    let uri = config[environment].api.v1_post_update_product.uri;
-    uri = uri.replace("${productid}", product.id);
+    let uri = config[environment].api.v1_patch_order_details.uri;
+    uri = uri.replace("${orderid}", order.id);
     return fetch(uri, {
-        method: 'POST',
+        method: 'PATCH',
         headers: myHeaders,
-        body: JSON.stringify(product),
+        body: JSON.stringify(order),
         redirect: 'follow'
     })
         .then(response => response.json())
         .then(response => response)
-        .catch(error => error)
+        .catch(error => error);
 };
 
-export default updateProductById;
-
+export default patchOrder;
