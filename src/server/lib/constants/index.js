fda4ce379cb5a6cc30f4b80d6b47a6faadaedc4b
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-05 10:18:19 +0530

    constants

--- src/server/lib/constants/index.js
+++ src/server/lib/constants/index.js
@@ -3,5 +3,5 @@ module.exports = {
     ADMIN_USER_KEY: 'tinnat',
     TINNAT_WEB_GUEST: 'tinnat_guest',
     TINNAT_WEB_GUEST_KEY: 'tinnat_guest_secret',
-    ORDER_LIFE_TIME: 14400
+    ORDER_LIFE_TIME: 14400000
 };
