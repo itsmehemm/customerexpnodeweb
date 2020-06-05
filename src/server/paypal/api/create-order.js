89ff955166af833a0e7d09755c1ac5827924246b
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-05 16:06:53 +0530

    Update create-order.js

--- src/server/paypal/api/create-order.js
+++ src/server/paypal/api/create-order.js
@@ -6,54 +6,7 @@ const { ENVIRONMENT_PRODUCTION } = require('../../lib/constants')
 const environment = args.env || ENVIRONMENT_PRODUCTION;
 const config = require('../../lib/config.json');
 
-const createOrder = async () => {
-    let order = {
-        "intent": "CAPTURE",
-        "payer": {
-            "name": {
-                "given_name": "Default",
-                "surname": "Default"
-            },
-            "address": {
-                "address_line_1": "123 Townsend St",
-                "address_line_2": "Floor 6",
-                "admin_area_2": "San Francisco",
-                "admin_area_1": "CA",
-                "postal_code": "94107",
-                "country_code": "US"
-            }
-        },
-        "purchase_units": [{
-            "reference_id": "#0000",
-            "description": "Item bought at Hemm Store",
-            "custom_id": "#1111",
-            "soft_descriptor": "",
-            "amount": {
-                "currency_code": "GBP",
-                "value": "81.04",
-                "breakdown": {
-                    "item_total": {
-                        "currency_code": "GBP",
-                        "value": "71.04"
-                    },
-                    "tax_total": {
-                        "currency_code": "GBP",
-                        "value": "10.00"
-                    }
-                }
-            },
-            "shipping": {
-                "address": {
-                    "address_line_1": "2211 N First Street",
-                    "address_line_2": "Building 17",
-                    "admin_area_2": "San Jose",
-                    "admin_area_1": "CA",
-                    "postal_code": "95131",
-                    "country_code": "US"
-                }
-            }
-        }]
-    };
+const createOrder = async (order) => {
     console.log('PAYPAL_API_CREATE_ORDER', `request: ${JSON.stringify(order)}`);
     const { create_order } = config.paypal[environment];
     try {
