0ef40ef2b2c4843f507e8d71f887e522ef874c97
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-07 16:32:34 +0530

    Update InstantPurchasePayment.js

--- src/client/components/payment/InstantPurchasePayment.js
+++ src/client/components/payment/InstantPurchasePayment.js
@@ -6,7 +6,6 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
 import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 import Header from '../header/Header';
