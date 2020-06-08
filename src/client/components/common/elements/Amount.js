44c174caf348b94c77db44ce88be1853dc7a1db9
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-08 20:36:37 +0530

    Update Amount.js

--- src/client/components/common/elements/Amount.js
+++ src/client/components/common/elements/Amount.js
@@ -2,33 +2,22 @@ import React from 'react';
 import { currencyCodeMapper } from '../../../lib/mappers';
 import {
     INSTANT_PERCENTAGE,
-    NO_DISCOUNT,
     INSTANT_AMOUNT
 } from '../../../lib/constants';
 
 const Amount = (props) => {
-    const { cost = {}, discount = {}, style = {} } = props;
-    let amount = parseInt(cost.amount);
-    const discountValue = parseInt(discount.value);
-    let discountedAmount;
-    if (discount.type === INSTANT_AMOUNT) {
-        discountedAmount = amount - discountValue;
-    }
-    if (discount.type === INSTANT_PERCENTAGE) {
-        discountedAmount = amount - (amount * discountValue / 100);
-    }
-    if (discount.type === NO_DISCOUNT) {
-        discountedAmount = amount;
-    }
+    const { amount, style } = props;
+    const { maximum_retail_price, discount, subtotal, correction, currency } = amount;
     return (
         <div className="amount-section" style={style}>
-            <span className="final-amount-text"> {currencyCodeMapper[cost.currency]}{discountedAmount} </span>
-            {discountedAmount !== amount && <span className="original-amount-text">{currencyCodeMapper[cost.currency]}{amount}</span>}
+            <span className="final-amount-text"> {currencyCodeMapper[currency]}{subtotal} </span>
+            {subtotal !== maximum_retail_price && <span className="original-amount-text">{currencyCodeMapper[currency]}{maximum_retail_price}</span>}
             {
-                discountValue !== 0 &&
+                parseInt(discount.value) > 0 &&
                 <span className="amount-offer-text">
-                    {discount.type === INSTANT_AMOUNT && ` Flat ${currencyCodeMapper[cost.currency]}${discountValue} off`}
                     {discount.type === INSTANT_PERCENTAGE && ` ${discountValue}% off`}
+                    {discount.type === INSTANT_AMOUNT && ` Flat ${currencyCodeMapper[currency]}${discount.value} off`}
+                    {discount.type === INSTANT_PERCENTAGE && ` ${discount.value}% off`}
                 </span>
             }
         </div>
