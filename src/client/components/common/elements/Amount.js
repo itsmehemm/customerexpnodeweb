9879a39e8eceace8cfb7df020dcac23e94696554
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-11 16:47:34 +0530

    Update Amount.js

--- src/client/components/common/elements/Amount.js
+++ src/client/components/common/elements/Amount.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import { currencyCodeMapper } from '../../../lib/mappers';
+import {
+    currencyCodeMapper
+} from '../../../lib/mappers';
 import {
     INSTANT_PERCENTAGE,
     INSTANT_AMOUNT
 } from '../../../lib/constants';
 
 const Amount = (props) => {
-    const { amount, style } = props;
+    const {
+        amount
+    } = props;
     if (!amount) return <div />
-    const { maximum_retail_price, discount, subtotal, correction, currency } = amount;
+    const {
+        maximum_retail_price,
+        discount,
+        subtotal,
+        currency
+    } = amount;
     return (
-        <div className="amount-section" style={style}>
+        <div>
             <span className="final-amount-text"> {currencyCodeMapper[currency]}{subtotal} </span>
             {subtotal !== maximum_retail_price && <span className="original-amount-text">{currencyCodeMapper[currency]}{maximum_retail_price}</span>}
             {
-                parseInt(discount.value) > 0 &&
+                parseInt(discount && discount.value) > 0 &&
                 <span className="amount-offer-text">
                     {discount.type === INSTANT_AMOUNT && ` Flat ${currencyCodeMapper[currency]}${discount.value} off`}
                     {discount.type === INSTANT_PERCENTAGE && ` ${discount.value}% off`}
