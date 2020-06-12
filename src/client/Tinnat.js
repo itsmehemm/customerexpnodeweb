5ee747d7050af06c249c905db1790770e6592f6f
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-12 09:25:54 +0530

    Update Tinnat.js

--- src/client/Tinnat.js
+++ src/client/Tinnat.js
@@ -8,6 +8,7 @@ import ContactUs from './components/common/ContactUs';
 import Cart from './components/cart/Cart';
 import InstantPurchase from './components/instant-purchase/InstantPurchase';
 import InstantPurchasePayment from './components/payment/InstantPurchasePayment';
+import InstantPurchaseConfirmation from './components/payment/InstantPurchaseConfirmation';
 import Checkout from './components/checkout/Checkout';
 import ProductDetailWrapper from './components/product-detail/ProductDetailWrapper';
 import Internal from './components/internal';
@@ -27,6 +28,7 @@ export default class Tinnat extends Component {
                 <Route exact path="/viewcart" component={Cart} />
                 <Route exact path="/checkout" component={Checkout} />
                 <Route exact path="/instant-purchase/payment/:orderid" component={InstantPurchasePayment} />
+                <Route exact path="/instant-purchase/complete/:orderid" component={InstantPurchaseConfirmation} />
                 <Route exact path="/instant-purchase/:orderid" component={InstantPurchase} />
                 <Route path="/internal" component={Internal} />
             </Router >
