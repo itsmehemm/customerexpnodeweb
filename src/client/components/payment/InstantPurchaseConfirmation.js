1c04d33627e7624e2996cc8f4a3b95c1ed10ee80
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-12 09:25:57 +0530

    Create InstantPurchaseConfirmation.js

---
+++ src/client/components/payment/InstantPurchaseConfirmation.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import Box from '@material-ui/core/Box';
+import Alert from '@material-ui/lab/Alert';
+import Typography from '../common/elements/Typography';
+import Header from '../header/Header';
+import ComponentLoader from '../common/loaders/ComponentLoader';
+import ViewAddress from '../instant-purchase/widgets/ViewAddress';
+import OrderSummary from '../instant-purchase/widgets/OrderSummary';
+import {
+    getOrderDetails
+} from '../../actions';
+import InstantOrderModal from '../../modals/instant-order/InstantOrderModal';
+import {
+    OPERATION_LOADING,
+    OPERATION_LOADING_COMPLETED,
+} from '../../lib/constants';
+
+export default class InstantPurchaseConfirmation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: OPERATION_LOADING
+        }
+    }
+
+    async componentDidMount() {
+        const orderid = this.props.match.params.orderid;
+        try {
+            const order = await getOrderDetails(orderid);
+            if (order && order.payment_information && order.payment_information.status === 'COMPLETED') {
+                const instantOrderModal = new InstantOrderModal();
+                instantOrderModal.updateData(order);
+                await this.setState({
+                    status: OPERATION_LOADING_COMPLETED,
+                    ...instantOrderModal.getData()
+                });
+            }
+        } catch (error) {
+
+        }
+    }
+
+    render() {
+        const {
+            status,
+            id,
+            billing_address,
+            personal_information,
+            purchase_items,
+            amount
+        } = this.state;
+        console.log(JSON.stringify(this.state));
+        return (
+            <>
+                <Header />
+                <Container>
+                    {
+                        status === OPERATION_LOADING_COMPLETED &&
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <Box m={2}>
+                                    <Alert severity="success">
+                                        Thank you, {billing_address && billing_address.name} <br></br>
+                                    Your order (Order ID: {id}) has been placed successfully.
+                                </Alert>
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Grid container>
+                                    <Grid item xs={7}>
+                                        <Box m={2}>
+                                            <Typography text={`Order Summary`} />
+                                        </Box>
+                                        <Box m={2}>
+                                            <Divider />
+                                        </Box>
+                                        <Box m={2}>
+                                            <OrderSummary
+                                                id={id}
+                                                name={purchase_items[0].data.name}
+                                                description={purchase_items[0].data.description}
+                                                amount={purchase_items[0].amount}
+                                                payment={amount}
+                                                discount={purchase_items[0].amount.discount}
+                                                size={purchase_items[0].size}
+                                                color={purchase_items[0].color}
+                                                quantity={purchase_items[0].quantity}
+                                                picture_links={purchase_items[0].picture_links}
+                                            />
+                                        </Box>
+                                    </Grid>
+                                    <Grid item xs={5}>
+                                        <Box m={2}>
+                                            <Typography text={`Your Order ID: ${id}`} />
+                                            <Typography text="Placed on Tue, 9 Jan, 2020" variant="caption" />
+                                        </Box>
+                                        <Box m={2}>
+                                            <Divider />
+                                        </Box>
+                                        <Box m={2}>
+                                            <Typography text="Items will be delivered to:" variant="subtitle2" />
+                                            <ViewAddress
+                                                {...billing_address}
+                                                forceShow={true}
+                                            />
+                                        </Box>
+                                        <Box m={2}>
+                                            <Divider />
+                                        </Box>
+                                        <Box m={2}>
+                                            <Typography text={`Check your email at ${personal_information && personal_information.email} for further information.`} variant="subtitle2" />
+                                        </Box>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    }
+                    {status === OPERATION_LOADING && <ComponentLoader />}
+                </Container>
+            </>
+        );
+    }
+}
