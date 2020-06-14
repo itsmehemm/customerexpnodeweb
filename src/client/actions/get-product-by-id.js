2e72558e7c8f693608c8ba572ba49fea37ce03d7
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-14 16:23:10 +0530

    Update get-product-by-id.js

--- src/client/actions/get-product-by-id.js
+++ src/client/actions/get-product-by-id.js
@@ -1,10 +1,12 @@
 import config from '../configs/config.json';
+import addProductToRecentView from './add-product-to-recent-view';
 
 const environment = config.environment;
 
 export const getProductById = (id) => {
     console.log('[INFO]', 'action::getProductById');
     console.log('[INFO]', 'request', `product_id=${id}`);
+    addProductToRecentView(id);
     var myHeaders = new Headers();
     myHeaders.append("X-TINNAT-SECURITY-CONTEXT", "{\"userId\": \"admin\", \"key\": \"tinnat\"}");
     return fetch(config[environment].api.v1_get_product_by_id.uri + id, {
