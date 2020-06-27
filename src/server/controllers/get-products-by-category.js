6190b6ef3ab416ae9d3f278073960ffbdd406dc0
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-27 12:28:54 +0530

    Update get-products-by-category.js

--- src/server/controllers/get-products-by-category.js
+++ src/server/controllers/get-products-by-category.js
@@ -18,7 +18,7 @@ const getProductsByCategory = (req, res) => {
                 if (Array.isArray(result) && result.length > 0) {
                     result.forEach(product => {
                         const viewProductModal = new ViewProductModal();
-                        viewProductModal.setData(product);
+                        viewProductModal.buildWithProduct(product);
                         products.push(viewProductModal.getData());
                     });
                 }
