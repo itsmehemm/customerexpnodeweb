515c8041082169bec01e660164d104f857d21b8e
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-27 12:29:02 +0530

    Update get-product-by-id.js

--- src/server/controllers/get-product-by-id.js
+++ src/server/controllers/get-product-by-id.js
@@ -5,7 +5,7 @@ const errorConstants = require('../lib/constants/error-constants');
 const getProductById = async (req, res) => {
     console.log(GET_PRODUCT_BY_ID_CONTROLLER, `processing request to get product by id: ${req.params.id}`);
     const viewProductModal = new ViewProductModal();
-    await viewProductModal.getProduct(req.params.id);
+    await viewProductModal.build(req.params.id);
     if (viewProductModal.getData()) {
         return res.status(200).send({
             ...viewProductModal.getData()
