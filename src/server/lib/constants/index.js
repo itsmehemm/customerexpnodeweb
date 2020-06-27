0956a9d6d4e075d7ee708d4ca8906dc6f4938c64
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-27 18:40:16 +0530

    Update index.js

--- src/server/lib/constants/index.js
+++ src/server/lib/constants/index.js
@@ -1,17 +1,17 @@
 module.exports = {
+    X_TINNAT_SECURITY_CONTEXT: 'x-tinnat-security-context',
     ADMIN_USER: 'admin',
     ADMIN_USER_KEY: 'tinnat',
+    TINNAT_WEB: 'tinnat_web',
     TINNAT_WEB_GUEST: 'tinnat_guest',
     TINNAT_WEB_GUEST_KEY: 'tinnat_guest_secret',
+    TINNAT_FACEBOOK_OAUTH: 'tinnat_facebook_oauth',
     ORDER_LIFE_TIME: 14400000,
     ENVIRONMENT_PRODUCTION: 'ENVIRONMENT_PRODUCTION',
     ENVIRONMENT_DEVELOPMENT: 'ENVIRONMENT_DEVELOPMENT',
-
     status: {
         COMPLETED: 'COMPLETED',
         SUCCESS: 'SUCCESS',
         FAILURE: 'FAILURE'
     },
-
-   
 };
