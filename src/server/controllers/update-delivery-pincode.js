77949a62dfe7ffba64c0c8f33dfff382803a28f4
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-28 18:25:17 +0530

    delivery time API

--- src/server/controllers/update-delivery-pincode.js
+++ src/server/controllers/update-delivery-pincode.js
@@ -6,19 +6,23 @@ const { UPDATE_DELIVERY_PINCODE_CONTROLLER } = require('../lib/constants/logging
 const updateDeliveryPincode = async (req, res, next) => {
     console.log(UPDATE_DELIVERY_PINCODE_CONTROLLER, `update pincode for delivery: ${req.body && req.body.pincode}`);
     if (req.body && req.body.pincode) {
-        const response = await postalpincodeApi.load(req.body.pincode);
-        if (!response || (response && response.error)) {
+        const address = await postalpincodeApi.load(req.body.pincode);
+        if (!address || (address && address.error)) {
             return res.status(404).send({
-                error: response.error
+                error: address.error
             });
         }
+        const delivery = await postalpincodeApi.getDeliveryStatus(address);
         req.session = req.session || {};
         req.session.delivery = {
             pincode: req.body.pincode,
-            state: response.getState(),
-            district: response.getDistrict(),
-            region: response.getRegion(),
-            place: response.getPlace()
+            state: address.getState(),
+            district: address.getDistrict(),
+            region: address.getRegion(),
+            place: address.getPlace(),
+            pincode: address.getPincode(),
+            status: delivery.status,
+            deliveryTime: delivery.deliveryTime
         };
         console.log(UPDATE_DELIVERY_PINCODE_CONTROLLER, `pincode details updated in session: ${JSON.stringify(req.session.delivery)}`);
         return res.status(200).send({
--- src/server/postalpincode/index.js
+++ src/server/postalpincode/index.js
@@ -1,5 +1,6 @@
 const request = require('async-request');
 const config = require('../lib/config.json');
+const deliveryData = require('./delivery-data.json');
 const errorConstants = require('../lib/constants/error-constants');
 
 const load = async (pincode) => {
@@ -21,7 +22,8 @@ const load = async (pincode) => {
                 getState: () => postoffice['State'],
                 getDistrict: () => postoffice['District'],
                 getRegion: () => postoffice['Region'],
-                getPlace: () => postoffice['Name']
+                getPlace: () => postoffice['Name'],
+                getPincode: () => pincode
             };
         } else {
             console.log('[POSTALPINCODE API]', 'no post offices found for pincode');
@@ -40,4 +42,56 @@ const load = async (pincode) => {
     };
 };
 
-module.exports = { load };
+const getDeliveryStatus = async (address) => {
+    if (!address || (address && address.error)) {
+        return {
+            status: 'NOT_DELIVERABLE',
+        };
+    }
+    let rule1 = 0, rule2 = 0, rule3 = 0;
+    let deliveryTime = 0;
+    let addressitems = ['India', address.getState(), address.getDistrict(), address.getRegion(), address.getPlace()];
+    addressitems.forEach(item => {
+        item = item.toLowerCase();
+        const data = deliveryData[item];
+        if (data) {
+            if (data.status === true) {
+                console.log('[POSTALPINCODE API]', 'getDeliveryStatus', `deliverable to ${item} in ${data.delivery_time} seconds`);
+                rule1 += 1;
+                deliveryTime = Math.max(deliveryTime, data.delivery_time);
+            } else if (data.status === false) {
+                console.log('[POSTALPINCODE API]', 'getDeliveryStatus', `not deliverable to ${item}`);
+                rule2 += 1;
+            } else {
+                console.log('[POSTALPINCODE API]', 'getDeliveryStatus', `data not found for ${item}`);
+                rule3 += 1;
+            }
+        } else {
+            console.log('[POSTALPINCODE API]', 'getDeliveryStatus', `data not found for ${item}`);
+            rule3 += 1;
+        }
+    });
+    console.log('[POSTALPINCODE API]', 'getDeliveryStatus', `rules: rule1=${rule1}; rule2=${rule2}; rule3=${rule3}`);
+    // if the place is deliverable, the rule is: zero in rule 2 and >= 1 in rule 1
+    // if rule1 = 0, rule 2 = 0 and rule 3 = 5, then the place is not discoverable. choose deliver by default.
+    if (rule1 >= 1 && rule2 === 0) {
+        return {
+            status: 'DELIVERABLE',
+            deliveryTime
+        };
+    }
+    if (rule3 === 5) {
+        return {
+            status: 'DELIVERABLE',
+            deliveryTime: 2678400 // max time in seconds = 31 days 
+        };
+    }
+    return {
+        status: 'NOT_DELIVERABLE'
+    };
+};
+
+module.exports = {
+    load,
+    getDeliveryStatus
+};
