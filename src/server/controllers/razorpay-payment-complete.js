da09bddf6a9b83adc745d08d3a7d7b01c2c10533
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-28 15:30:40 +0530

    Update razorpay-payment-complete.js

--- src/server/controllers/razorpay-payment-complete.js
+++ src/server/controllers/razorpay-payment-complete.js
@@ -1,7 +1,13 @@
+const Razorpay = require('razorpay');
+const args = require('yargs').argv;
+const mailservices = require('tinnat-mailservices')
 const { RAZORPAY_PAYMENT_COMPLETE_NOTIFICATION } = require('../lib/constants/logging-constants');
+const { ENVIRONMENT_PRODUCTION } = require('../lib/constants')
 const InstantPurchaseModal = require('../modals/InstantPurchaseModal');
 const errorConstants = require('../lib/constants/error-constants');
 const apiMessages = require('../lib/constants/api-messages');
+const config = require('../lib/config.json');
+const environment = args.env || ENVIRONMENT_PRODUCTION;
 
 const razorpayPaymentComplete = async (req, res) => {
     console.log(RAZORPAY_PAYMENT_COMPLETE_NOTIFICATION, `processing request to complete payment for order id: ${req.params.id}.`);
@@ -24,19 +30,47 @@ const razorpayPaymentComplete = async (req, res) => {
             error: errorConstants.RAZORPAY_PAYMENT_FAILED
         });
     }
-    await instantPurchaseModal.updatePaymentDetails({
-        status: "COMPLETED",
-        transaction_id: payment_id,
-        processor: 'RAZORPAY',
-        processor_order_id: order_id,
-        others: {
-            signature: signature
-        }
+    const razorPay = new Razorpay({
+        key_id: config.razorpay[environment].api_key,
+        key_secret: config.razorpay[environment].api_secret
     });
-    return res.status(200).send({
-        ...apiMessages.PAYMENT_COMPLETED,
-        ...instantPurchaseModal.getOrder(),
-    });
+    return razorPay
+        .payments
+        .fetch(payment_id)
+        .then(async response => {
+            console.log(RAZORPAY_PAYMENT_COMPLETE_NOTIFICATION, `payment information fetched from razorpay: ${JSON.stringify(response)}`);
+            if (response && response.id && response.order_id) {
+                await instantPurchaseModal.updatePaymentDetails({
+                    status: "COMPLETED",
+                    transaction_id: payment_id,
+                    processor: 'RAZORPAY',
+                    processor_order_id: order_id,
+                    others: {
+                        signature: signature
+                    }
+                });
+                const service = await mailservices.build('noreply');
+                const order = instantPurchaseModal.getOrder();
+                const to = order && order.personal_information && order.personal_information.email;
+                if (to) {
+                    service.send(to, 'Payment received', JSON.stringify(order));
+                }
+                return res.status(200).send({
+                    ...apiMessages.PAYMENT_COMPLETED,
+                    ...instantPurchaseModal.getOrder(),
+                });
+            } else {
+                return res.status(400).send({
+                    ...apiMessages.PAYMENT_FAILED
+                });
+            }
+        })
+        .catch(error => {
+            console.log(RAZORPAY_PAYMENT_COMPLETE_NOTIFICATION, `error receiving payment information from razorpay: ${JSON.stringify(error)}`);
+            return res.status(400).send({
+                ...apiMessages.PAYMENT_FAILED
+            });
+        });
 };
 
 module.exports = razorpayPaymentComplete;
