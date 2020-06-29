3d288e0907a62b109edaf42663e1132318f6eb09
itsmehemm <hemanthprasathmurali@gmail.com>
2020-06-29 09:52:10 +0530

    optimization

--- src/server/controllers/get-order-by-id.js
+++ src/server/controllers/get-order-by-id.js
@@ -1,15 +1,43 @@
+const postalpincodeApi = require('../postalpincode');
 const { GET_ORDER_BY_ID_CONTROLLER } = require('../lib/constants/logging-constants');
 const InstantPurchaseModal = require('../modals/InstantPurchaseModal');
 const errorConstants = require('../lib/constants/error-constants');
+const { constructDeliveryObj } = require('../lib/utils');
+const { ORDER_COMPLETED } = require('../lib/constants');
+const { response } = require('express');
 
 const getOrderById = async (req, res) => {
     console.log(GET_ORDER_BY_ID_CONTROLLER, `processing request to get order by id: ${req.params.id}.`);
     const instantPurchaseModal = new InstantPurchaseModal();
-    instantPurchaseModal.get(req.params.id);
+    instantPurchaseModal.load(req.params.id);
     if (instantPurchaseModal.getOrderId()) {
+        let delivery = null;
+        if (instantPurchaseModal.getStatus() !== ORDER_COMPLETED) {
+            const shippingAddress = instantPurchaseModal.getShippingAddress();
+            if (shippingAddress && shippingAddress.pincode) {
+                const address = await postalpincodeApi.load(shippingAddress.pincode);
+                if (address && address.getPincode()) {
+                    let deliveryData = await postalpincodeApi.getDeliveryStatus(address);
+                    delivery = constructDeliveryObj({
+                        state: address.getState(),
+                        district: address.getDistrict(),
+                        region: address.getRegion(),
+                        place: address.getPlace(),
+                        pincode: address.getPincode(),
+                        status: deliveryData.status,
+                        deliveryTime: deliveryData.deliveryTime
+                    });
+                } else if (response && response.error) {
+                    delivery = {
+                        error: response.error
+                    };
+                }
+            }
+        }
         return res.send({
             id: instantPurchaseModal.getOrderId(),
-            ...instantPurchaseModal.getOrder()
+            ...instantPurchaseModal.getOrder(),
+            delivery: delivery
         });
     } else {
         return res.status(404).send({
--- src/server/controllers/patch-order-by-id.js
+++ src/server/controllers/patch-order-by-id.js
@@ -1,20 +1,21 @@
-const { PATCH_ORDER_BY_ID_CONTROLLER } = require('../lib/constants/logging-constants');
 const InstantPurchaseModal = require('../modals/InstantPurchaseModal');
-const errorConstants = require('../lib/constants/error-constants');
+const { COMPLETED } = require('../lib/constants');
 const apiMessages = require('../lib/constants/api-messages');
+const errorConstants = require('../lib/constants/error-constants');
+const { PATCH_ORDER_BY_ID_CONTROLLER } = require('../lib/constants/logging-constants');
 
 const patchOrderById = async (req, res) => {
-    console.log(PATCH_ORDER_BY_ID_CONTROLLER, `processing request to patch order by id: ${req.params.id}.`);
-    console.log(PATCH_ORDER_BY_ID_CONTROLLER, `patch request received: ${JSON.stringify(req.body)}.`);
+    console.log(PATCH_ORDER_BY_ID_CONTROLLER, `processing request to patch order by id: ${req.params.id}`);
+    console.log(PATCH_ORDER_BY_ID_CONTROLLER, `patch request received: ${JSON.stringify(req.body)}`);
     const instantPurchaseModal = new InstantPurchaseModal();
-    instantPurchaseModal.get(req.params.id);
+    instantPurchaseModal.load(req.params.id);
     if (!instantPurchaseModal.getOrderId()) {
         return res.status(404).send({
             error: errorConstants.ORDER_NOT_FOUND
         });
     }
-    if (instantPurchaseModal.getOrder().payment_information &&
-        instantPurchaseModal.getOrder().payment_information.status === 'COMPLETED') {
+    if (instantPurchaseModal.getPaymentInformation() &&
+        instantPurchaseModal.getPaymentInformation().status === COMPLETED) {
         return res.status(404).send({
             error: errorConstants.CANNOT_PATCH_COMPLETED_ORDER
         });
@@ -27,7 +28,11 @@ const patchOrderById = async (req, res) => {
         return res.status(200).send({
             ...apiMessages.ORDER_PATCHED,
             id: instantPurchaseModal.getOrderId(),
-            instant_payment_url: `/instant-purchase/payment/${orderId}`
+            links: [{
+                name: 'PAYMENT',
+                method: 'GET',
+                href: `/instant-purchase/payment/${orderId}`
+            }]
         });
     } else {
         return res.status(500).send({
