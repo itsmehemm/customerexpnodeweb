4005d522870182ddd2592bbf180a2b5537378dd8
itsmehemm <hemanthprasathmurali@gmail.com>
2020-07-01 22:03:21 +0530

    Update Typography.js

--- src/client/components/common/elements/Typography.js
+++ src/client/components/common/elements/Typography.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Icon from '../elements/Icon';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 const _Typography = (props) => {
     const {
         width = 12,
         icon,
+        icon_type = "default",
         icon_pos = "front",
-        text, style,
+        text, style, iconStyle,
         align,
         size = "h6",
         component = "h2"
@@ -22,8 +24,9 @@ const _Typography = (props) => {
                 component={component}
                 {...props}
             >
-                {icon_pos === 'front' && icon && <> <Icon name={icon} /> {text} </>}
-                {icon_pos === 'back' && icon && <> {text} <Icon name={icon} /> </>}
+                {icon_type === 'default' && icon_pos === 'front' && icon && <> <Icon name={icon} /> {text} </>}
+                {icon_type === 'default' && icon_pos === 'back' && icon && <> {text} <Icon name={icon} /> </>}
+                {icon_type === 'user' && icon === 'facebook' && <> <FacebookIcon style={iconStyle} /> {text} </>}
                 {!icon && text}
             </Typography>
         </Grid>
