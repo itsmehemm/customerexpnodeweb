ea4d6ba145f8d823759062725245b1972244d759
itsmehemm <hemanthprasathmurali@gmail.com>
2020-07-01 22:02:46 +0530

    Update create-session-with-facebook.js

--- src/server/controllers/create-session-with-facebook.js
+++ src/server/controllers/create-session-with-facebook.js
@@ -1,11 +1,12 @@
 const uniqid = require('uniqid');
+const userserv = require('tinnat-userserv');
 const args = require('yargs').argv;
 const facebookOAuth = require('../oauth/facebook');
 const config = require('../lib/config.json');
 const { CREATE_SESSION_WITH_FACEBOOK_CONTROLLER } = require('../lib/constants/logging-constants');
 const {
-    TINNAT_FACEBOOK_OAUTH,
-    ENVIRONMENT_PRODUCTION
+    ENVIRONMENT_PRODUCTION,
+    PERSONAL_ACCOUNT,
 } = require('../lib/constants/');
 const environment = args.env || ENVIRONMENT_PRODUCTION;
 
@@ -15,14 +16,27 @@ const createSessionWithFacebook = async (req, res, next) => {
     console.log(CREATE_SESSION_WITH_FACEBOOK_CONTROLLER, `fb user access token': ${userAccessToken}`);
     const user = await facebookOAuth.authenticate(userAccessToken);
     if (user) {
-        req.user = user;
-        req.session.userId = user.accountNumber;
-        req.session.user = user;
-        req.session.sessionId = uniqid('SESSION-').toUpperCase();
-        req.session.startTime = new Date();
-        req.session.loginProvider = TINNAT_FACEBOOK_OAUTH;
-        const redirectUrl = req.query.redirect_url || config.tinnat[environment].url.login_success;
-        return res.redirect(redirectUrl);
+        const tinnatUser = {
+            name: user.name,
+            email: user.email,
+            party_id: user.accountNumber,
+            account_type: PERSONAL_ACCOUNT
+        };
+        const response = await userserv.createFacebookUser(tinnatUser);
+        if (response && response.data) {
+            req.user = {
+                name: response.data.name,
+                email: response.data.email,
+                accountType: response.data.account_type,
+                authenticationParty: response.data.authentication_party,
+                accountId: response.data.account_id
+            };
+            req.session.user = req.user;
+            req.session.sessionId = uniqid('SESSION-').toUpperCase();
+            req.session.startTime = new Date().getTime();
+            const redirectUrl = req.query.redirect_url || config.tinnat[environment].url.login_success;
+            return res.redirect(redirectUrl);
+        }
     }
     const errorUrl = req.query.error_url || config.tinnat[environment].url.login_failure;
     return res.redirect(errorUrl);
