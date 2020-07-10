d61a391dd4da3a7546a7e8440e135393ad40ccba
itsmehemm <hemanthprasathmurali@gmail.com>
2020-07-10 12:52:18 +0530

    Update common.js

--- src/client/lib/handlers/common.js
+++ src/client/lib/handlers/common.js
@@ -4,13 +4,13 @@ import {
 } from '../error-constants';
 
 export const commonResponseHandler = (response) => {
-    if (response && response.error && response.error.message === PERMISSION_DENIED) {
-        console.log('commonResponseHandler', `user not permitted to access this page.`);
-        window.location.href = '/notfound';
-    }
     if (response && response.error && response.error.message === UNAUTHORIZED_REQUEST) {
         console.log('commonResponseHandler', `user not authorized to access this page.`);
         window.location.href = '/login';
+    }
+    if (response && response.error && response.error.message === PERMISSION_DENIED) {
+        console.log('commonResponseHandler', `user not permitted to access this page.`);
+        window.location.href = '/notfound';
     }
     return response;
 };
