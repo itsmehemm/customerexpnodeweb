67ab04d52b634553814938021b6c4dc2a70b68cd
itsmehemm <hemanthprasathmurali@gmail.com>
2020-07-13 21:24:44 +0530

    header

--- src/client/lib/request/get-headers.js
+++ src/client/lib/request/get-headers.js
@@ -1,8 +1,12 @@
-const getAPIHeaders = () => {
+export const getAPIHeaders = () => {
     let apiHeaders = new Headers();
     apiHeaders.append("X-TINNAT-SECURITY-CONTEXT", "{\"clientId\": \"tinnat_web\", \"key\": \"tinnat_guest_secret\"}");
     apiHeaders.append("Content-Type", "application/json");
     return apiHeaders;
 };
 
-export default getAPIHeaders;
+export const getDefaultAPIHeaders = () => {
+    let apiHeaders = new Headers();
+    apiHeaders.append("Content-Type", "application/json");
+    return apiHeaders;
+};
