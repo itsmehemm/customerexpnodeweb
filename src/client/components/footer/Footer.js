1aa00771c80ca9c10c01cb60018b02db05042916
itsmehemm <hemanthprasathmurali@gmail.com>
2020-07-15 18:08:38 +0530

    Update Footer.js

--- src/client/components/footer/Footer.js
+++ src/client/components/footer/Footer.js
@@ -17,53 +17,53 @@ const Footer = () => {
                 </Grid>
                 <Grid item xs={2}>
                     <Box m={2}>
-                        <Typography variant="button" text="Quick Links" />
-                        <Typography variant="body2" className="t-text-link-3" text="Home" />
-                        <Typography variant="body2" className="t-text-link-3" text="All Products" />
-                        <Typography variant="body2" className="t-text-link-3" text="Contact Us" />
-                        <Typography variant="body2" className="t-text-link-3" text="About Us" />
-                        <Typography variant="body2" className="t-text-link-3" text="Help" />
+                        <Typography variant='button' text='Quick Links' />
+                        <Typography variant='body2' className='t-text-link-3' text='Home' onClick={() => window.href = '/home'} />
+                        <Typography variant='body2' className='t-text-link-3' text='All Products' onClick={() => window.href = '/products'} />
+                        <Typography variant='body2' className='t-text-link-3' text='Contact Us' onClick={() => window.href = '/contactus'} />
+                        <Typography variant='body2' className='t-text-link-3' text='About Us' onClick={() => window.href = '/about'} />
+                        <Typography variant='body2' className='t-text-link-3' text='Help' onClick={() => window.href = '/help'} />
                     </Box>
                 </Grid>
                 <Grid item xs={2}>
                     <Box m={2}>
-                        <Typography variant="button" text="Social" />
-                        <Typography variant="body2" className="t-text-link-3" text="Facebook" />
-                        <Typography variant="body2" className="t-text-link-3" text="Instagram" />
-                        <Typography variant="body2" className="t-text-link-3" text="Youtube" />
+                        <Typography variant='button' text='Social' />
+                        <Typography variant='body2' className='t-text-link-3' text='Facebook' onClick={() => window.open('https://www.facebook.com/tinnatshirts')} />
+                        <Typography variant='body2' className='t-text-link-3' text='Instagram' onClick={() => window.open('https://www.instagram.com/tinnat_store/')} />
+                        <Typography variant='body2' className='t-text-link-3' text='Youtube' onClick={() => window.open('https://www.facebook.com/tinnatshirts')} />
                     </Box>
                 </Grid>
                 <Box m={2}>
-                    <Divider orientation="vertical" style={{ backgroundColor: '#fff' }} />
+                    <Divider orientation='vertical' style={{ backgroundColor: '#fff' }} />
                 </Box>
                 <Grid item xs={2}>
                     <Box m={2}>
-                        <Typography variant="body1" text="Mail Us:" />
-                        <Typography variant="body2" text="Tinnos India Private Limited," />
-                        <Typography variant="body2" text="2/153, 2nd block, 2nd floor" />
-                        <Typography variant="body2" text="Mogappair west" />
-                        <Typography variant="body2" text="Chennai, 600 037" />
-                        <Typography variant="body2" text="Tamil Nadu, India" />
+                        <Typography variant='body1' text='Mail Us:' />
+                        <Typography variant='body2' text='Tinnos India Private Limited,' />
+                        <Typography variant='body2' text='2/153, 2nd block, 2nd floor' />
+                        <Typography variant='body2' text='Mogappair west' />
+                        <Typography variant='body2' text='Chennai, 600 037' />
+                        <Typography variant='body2' text='Tamil Nadu, India' />
                     </Box>
                 </Grid>
                 <Grid item xs={2}>
                     <Box m={2}>
-                        <Typography variant="body1" text="Registered Address:" />
-                        <Typography variant="body2" text="Tinnos India Private Limited," />
-                        <Typography variant="body2" text="2/153, 2nd block, 2nd floor" />
-                        <Typography variant="body2" text="Mogappair west" />
-                        <Typography variant="body2" text="Chennai, 600 037" />
-                        <Typography variant="body2" text="Tamil Nadu, India" />
-                        <Typography variant="body2" text="GST: 33AOHPL2182F1Z6" />
-                        <Typography variant="body2" text="Phone: +91 87787 46089" />
+                        <Typography variant='body1' text='Registered Address:' />
+                        <Typography variant='body2' text='Tinnos India Private Limited,' />
+                        <Typography variant='body2' text='2/153, 2nd block, 2nd floor' />
+                        <Typography variant='body2' text='Mogappair west' />
+                        <Typography variant='body2' text='Chennai, 600 037' />
+                        <Typography variant='body2' text='Tamil Nadu, India' />
+                        <Typography variant='body2' text='GST: 33AOHPL2182F1Z6' />
+                        <Typography variant='body2' text='Phone: +91 87787 46089' />
                     </Box>
                 </Grid>
                 <Grid item xs={12}>
                     <Divider style={{ backgroundColor: '#fff' }} />
                 </Grid>
                 <Grid item xs={12}>
-                    <Box m={2} align="center">
-                        <Typography variant="body2" text={`© 2020 Tinnos India Private Limited`} />
+                    <Box m={2} align='center'>
+                        <Typography variant='body2' text={`© ${new Date().getFullYear()} Tinnos India Private Limited`} />
                     </Box>
                 </Grid>
             </Grid>
