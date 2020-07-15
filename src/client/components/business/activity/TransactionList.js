bd991fa1cf94f4d05d2b9a97b30a135d40b79484
itsmehemm <hemanthprasathmurali@gmail.com>
2020-07-15 18:08:41 +0530

    Update TransactionList.js

--- src/client/components/business/activity/TransactionList.js
+++ src/client/components/business/activity/TransactionList.js
@@ -40,32 +40,32 @@ const TransactionList = ({ transactions, columns = {} }) => {
     const classes = useStyles();
     return (
         <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="customized table">
+            <Table className={classes.table} aria-label='customized table'>
                 <TableHead>
                     <TableRow>
-                        {columns.date && <StyledTableCell align="center"><Typography variant="body1_bold" text="Date" /></StyledTableCell>}
-                        {columns.transaction_id && <StyledTableCell align="center"><Typography variant="body1_bold" text="Transaction ID" /></StyledTableCell>}
-                        {columns.order_id && <StyledTableCell align="center"><Typography variant="body1_bold" text="Order ID" /></StyledTableCell>}
-                        {columns.customer && <StyledTableCell align="center"><Typography variant="body1_bold" text="Customer" /></StyledTableCell>}
-                        {columns.payment_status && <StyledTableCell align="center"><Typography variant="body1_bold" text="Payment Status" /></StyledTableCell>}
-                        {columns.gross && <StyledTableCell align="center"><Typography variant="body1_bold" text="Gross" /></StyledTableCell>}
-                        {columns.actions && <StyledTableCell align="center"><Typography variant="body1_bold" text="Actions" /></StyledTableCell>}
+                        {columns.date && <StyledTableCell align='center'><Typography variant='button' text='Date' /></StyledTableCell>}
+                        {columns.transaction_id && <StyledTableCell align='center'><Typography variant='button' text='Transaction ID' /></StyledTableCell>}
+                        {columns.order_id && <StyledTableCell align='center'><Typography variant='button' text='Order ID' /></StyledTableCell>}
+                        {columns.customer && <StyledTableCell align='center'><Typography variant='button' text='Customer' /></StyledTableCell>}
+                        {columns.payment_status && <StyledTableCell align='center'><Typography variant='button' text='Payment Status' /></StyledTableCell>}
+                        {columns.gross && <StyledTableCell align='center'><Typography variant='button' text='Gross' /></StyledTableCell>}
+                        {columns.actions && <StyledTableCell align='center'><Typography variant='button' text='Actions' /></StyledTableCell>}
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {transactions.map((row) => (
                         <StyledTableRow key={row.transaction_id}>
-                            {columns.date && <StyledTableCell align="left"><Typography variant="body2" text={moment(row.time_stamp).format("MMM D, YYYY")} /></StyledTableCell>}
-                            {columns.transaction_id && <StyledTableCell align="center"><Typography variant="body2" text={row.transaction_id} /></StyledTableCell>}
-                            {columns.order_id && <StyledTableCell align="center"><Typography variant="body2" text={row.order_id} /></StyledTableCell>}
-                            {columns.customer && <StyledTableCell align="left">
-                                {row.billing_address && row.billing_address.name && <Typography variant="body2" text={row.billing_address.name} />}
-                                {row.personal_information && row.personal_information.email && <Typography variant="body2" text={row.personal_information.email} />}
-                                {row.personal_information && row.personal_information.phone_number && <Typography variant="body2" text={row.personal_information.phone_number} />}
+                            {columns.date && <StyledTableCell align='center'><Typography variant='body2' text={moment(row.time_stamp).format('MMM D, YYYY / MM:HH:SS')} /></StyledTableCell>}
+                            {columns.transaction_id && <StyledTableCell align='center'><Typography variant='body2' text={row.transaction_id} /></StyledTableCell>}
+                            {columns.order_id && <StyledTableCell align='center'><Typography variant='body2' text={row.order_id} /></StyledTableCell>}
+                            {columns.customer && <StyledTableCell align='left'>
+                                {row.billing_address && row.billing_address.name && <Typography variant='body2' text={row.billing_address.name} />}
+                                {row.personal_information && row.personal_information.email && <Typography variant='body2' text={row.personal_information.email} />}
+                                {row.personal_information && row.personal_information.phone_number && <Typography variant='body2' text={row.personal_information.phone_number} />}
                             </StyledTableCell>}
-                            {columns.payment_status && <StyledTableCell align="center"><Typography variant="body2" text={row.status} /></StyledTableCell>}
-                            {columns.gross && <StyledTableCell align="center"><Typography variant="body2" text={`${currencyCodeMapper[row.amount.currency]}${row.amount.subtotal}`} /></StyledTableCell>}
-                            {columns.actions && <StyledTableCell align="center"><Button onClick={() => window.open(`/business/activity/transaction/${row.transaction_id}`)}>View</Button></StyledTableCell>}
+                            {columns.payment_status && <StyledTableCell align='center'><Typography variant='body2' text={row.status} /></StyledTableCell>}
+                            {columns.gross && row.amount && <StyledTableCell align='center'><Typography variant='body2' text={`${currencyCodeMapper[row.amount.currency]}${row.amount.subtotal}`} /></StyledTableCell>}
+                            {columns.actions && <StyledTableCell align='center'><Button onClick={() => window.open(`/business/activity/transaction/${row.transaction_id}`)}>View</Button></StyledTableCell>}
                         </StyledTableRow>
                     ))}
                 </TableBody>
