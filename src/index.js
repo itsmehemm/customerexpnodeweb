c1b870bcc9f4d96519a66af7fd502937050e6d00
Hemanth M <hemanthprasathmurali@gmail.com>
2020-09-12 12:29:42 +0530

    Revert "Added test commits"
    This reverts commit f67b6fe2cabdf903967b1d372c2b878516130eae.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Tinnat from './client/Tinnat';
 
-// This is a sample comment
 render(
     <Router>
         <Tinnat />
